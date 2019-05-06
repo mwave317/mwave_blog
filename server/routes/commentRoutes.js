11a71d1dc3642a963e57419e2663f39324f571b1
Ken Coppola <mwave317@gmail.com>
2019-05-06 18:03:42 -0400

    Added the route to add a comment.

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -1,7 +1,18 @@
 const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require('mongoose');
+const Comment = mongoose.model('comment');
 
 module.exports = app => {
     app.post('/api/comment', requireLogin, (req, res) => {
+        const { comment, timestamp, verified } = req.body;
 
+        const addComment = new Comment ({
+            comment,
+            timestamp,
+            verified,
+
+        });
+        console.log('Do you see the comment', addComment);
+        addComment.save();
     });
 }
