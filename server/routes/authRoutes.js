295e66ea1d14f64ade34672650abd45d40b9cccf
Ken Coppola <mwave317@gmail.com>
2019-05-07 17:22:41 -0400

    Changed the url for the callback from google.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -11,7 +11,7 @@ module.exports = app => {
     );
     
     app.get(
-        '/auth/google/callback', passport.authenticate('google'),
+        'https://mwave317.herokuapp.com/auth/google/callback', passport.authenticate('google'),
         (req, res) => {
             res.redirect('/home');
         }
